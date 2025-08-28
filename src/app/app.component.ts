ab78d7d746acb8f1c8266d13a808c50fd3203e46
test <test@tests-MacBook-Pro.local>
2025-08-28 08:16:01 +0530

    App level injection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { SubscribeService } from './services/subscribe.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  providers:[SubscribeService]
 })
 export class AppComponent {
   title = 'angular-services-dependency-injection';
